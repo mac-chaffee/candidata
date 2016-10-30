5fb294c44d2446abade659dc802f2825a53f3e36
mac-chaffee <macchaffee88@gmail.com>
2016-10-30 04:30:15 -0400

    Changed paths for heroku again

--- candidata/candidata/settings.py
+++ candidata/candidata/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'candidata', 'candidata', 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'candidata', 'static'), ]
 # Simplified static file serving. https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
