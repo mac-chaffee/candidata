26773c42d05f1c3d92d51b6fe4756385a38d6331
RJ Duane <git@rjd.io>
2016-10-30 03:40:26 -0400

    Add 538 polling average API

---
+++ candidata/candidata/fivethirtyeight.py
@@ -0,0 +1,20 @@
+import requests
+
+# correct discrepencies betwwen APIs
+candidateMap = {
+    "hillary-clinton": "D",
+    "donald-trump": "R",
+    "gary-johnson": "L"
+}
+
+def getPollNum(candidateString):
+
+    # set up API call
+    r = requests.get("http://projects.fivethirtyeight.com/2016-election-forecast/US.json")
+    r = r.json()
+
+    # get current polling average
+    currentPollNum = r["forecasts"]["latest"][candidateMap[candidateString]]["models"]["polls"]["forecast"]
+
+    print(currentPollNum)
+    return currentPollNum
---
+++ candidata/candidata/politifact.py
@@ -0,0 +1,43 @@
+import requests
+
+# correct discrepencies betwwen APIs
+candidateMap = {
+    "hillary-clinton": "hillary-clinton",
+    "donald-trump": "donald-trump",
+    "gary-johnson": "gary-johnson"
+}
+issueMap = {
+    "Health Care": "health-care",
+    "Economy": "economy",
+    "Terrorism": "terrorism",
+    "Environment": "environment"
+}
+
+def getRecentStatementRulings(candidateString, issueString):
+
+    # object for storing statements and rulings
+    statementsAndRulings = {}
+
+    # set up API call
+    args = {
+        "order_by": "ruling_date",
+        "speaker__name_slug": candidateString,
+        "subject__subject_slug": issueMap[issueString],
+        "limit": 5,
+        "format": "json"
+    }
+    r = requests.get("http://www.politifact.com/api/v/2/statement/", args)
+    r = r.json()
+
+    # loop through the 5 statements/rulings we want to use
+    for i in range(len(r["objects"])):
+
+        # get the statements and rulings...
+        statement = r ["objects"] [i] ["statement"]
+        ruling = r ["objects"] [i] ["ruling"] ["ruling"]
+
+        # ...and put them in the object
+        statementsAndRulings [i] = [statement, ruling]
+
+    print(statementsAndRulings)
+    return statementsAndRulings
