c6d28b6ea5d93faee4e1a54990a123221fd2067b
mac-chaffee <macchaffee88@gmail.com>
2016-10-29 20:06:05 -0400

    Fixed static files path

--- candidata/candidata/settings.py
+++ candidata/candidata/settings.py
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'candidata', 'static'), ]
+# Simplified static file serving. https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
